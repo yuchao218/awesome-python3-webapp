bbae4e297944ccdb75fd4896cdf78410f0a20990
yuchao218 <yuchao218@126.com>
2019-11-06 14:32:44 +0800

    edit config_default.py

--- www/config_default.py
+++ www/config_default.py
@@ -12,8 +12,8 @@ configs = {
     'db': {
         'host': '127.0.0.1',
         'port': 3306,
-        'user': 'www',
-        'password': 'www',
+        'user': 'www-data',
+        'password': 'yc1234',
         'db': 'awesome'
     },
     'session': {
